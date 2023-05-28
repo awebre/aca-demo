a626453878c8dc236ac1512c0adf7d847a0ee7c8
Austin Webre <austin@thewebres.com>
2023-05-28 09:12:56 -0500

    (MINOR) test

--- ACADemo.API/Program.cs
+++ ACADemo.API/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Hello World!\n\nThis is a new thing.");
 
 app.Run();
